6673d175450c2bd76bd70652be5ea3ae4bdc131f
Helder Burato Berto <helder.burato@gmail.com>
2022-08-10 11:20:24 +0100

    Add docs to word_list, random_word

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,9 +1,33 @@
 defmodule Dictionary do
+  @moduledoc """
+  This is the Dictionary module
+  """
+
+  @doc """
+  Prints a list of words
+
+  Returns: `:ok`
+
+  ## Examples
+    iex> Dictionary.word_list()
+    :ok
+
+  """
   def word_list do
     words = File.read!("assets/words.txt")
     String.split(words, ~r/\n/, trim: true)
   end
 
+  @doc """
+  Prints a random word from words list
+
+  Returns: `:ok`
+
+  ## Examples
+    iex> Dictionary.random_word()
+    :ok
+
+  """
   def random_word do
     Enum.random(word_list())
   end
