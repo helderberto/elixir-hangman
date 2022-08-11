b853f8d5469300dbdf24e824ce265f225ee6de3c
Helder Burato Berto <helder.burato@gmail.com>
2022-08-11 15:02:34 +0100

    Add Lists example
    Using some pattern matching and recursion

---
+++ dictionary/lib/lists.ex
@@ -0,0 +1,17 @@
+defmodule Lists do
+  def swap({a, b}) do
+    {b, a}
+  end
+
+  def equal(a, a), do: true
+  def equal(_, _), do: false
+
+  def sum([]), do: 0
+  def sum([h | t]), do: h + sum(t)
+
+  def double([]), do: []
+  def double([h | t]), do: [h * 2 | double(t)]
+
+  def square([]), do: []
+  def square([h | t]), do: [h * h | square(t)]
+end
