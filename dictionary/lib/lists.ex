d13764157860ba8d4cf87c1cf3871710505d320e
Helder Burato Berto <helder.burato@gmail.com>
2022-08-11 15:18:03 +0100

    Add Lists.map()

--- dictionary/lib/lists.ex
+++ dictionary/lib/lists.ex
@@ -14,4 +14,7 @@ defmodule Lists do
 
   def square([]), do: []
   def square([h | t]), do: [h * h | square(t)]
+
+  def map([], _func), do: []
+  def map([h|t], func), do: [func.(h) | map(t, func)]
 end
