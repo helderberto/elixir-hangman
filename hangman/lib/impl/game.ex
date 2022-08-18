d4ede3a4b3e1fb296f4e6e1acdf668ebcf328dda
Helder Burato Berto <helder.burato@gmail.com>
2022-08-18 11:05:40 +0100

    Define types for Game

--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -1,4 +1,11 @@
 defmodule Hangman.Impl.Game do
+  @type t :: %Hangman.Impl.Game{
+          turns_left: integer,
+          game_state: Hangman.state(),
+          letters: list(String.t()),
+          used: MapSet.t(String.t())
+        }
+
   defstruct(
     turns_left: 7,
     game_state: :initializing,
