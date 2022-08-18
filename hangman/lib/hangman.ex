64472a744a26512849af82da9f43a60cfcad6a75
Helder Burato Berto <helder.burato@gmail.com>
2022-08-18 15:44:36 +0100

    Move types to a new file
    We've some shared types, so that make sense to split in a new file.

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -2,21 +2,15 @@ defmodule Hangman do
   # Get the last part of the path, on this case "Game" will be the alias
   # or you can set with `alias Hangman.Impl.Game, as: Game`
   alias Hangman.Impl.Game
+  alias Hangman.Type
 
-  @type state :: :initializing | :won | :lost | :good_guess | :bad_guess | :already_used
   @opaque game :: Game.t()
-  @type tally :: %{
-          turns_left: integer,
-          game_state: state,
-          letters: list(String.t()),
-          used: list(String.t())
-        }
 
   @spec new_game() :: game
   def new_game
   defdelegate new_game, to: Game
 
-  @spec make_move(game, String.t()) :: {game, tally}
+  @spec make_move(game, String.t()) :: {game, Type.tally}
   def make_move(_game, _guess) do
   end
 end
--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -1,7 +1,9 @@
 defmodule Hangman.Impl.Game do
+  alias Hangman.Type
+
   @type t :: %__MODULE__{
           turns_left: integer,
-          game_state: Hangman.state(),
+          game_state: Type.state(),
           letters: list(String.t()),
           used: MapSet.t(String.t())
         }
@@ -21,7 +23,7 @@ defmodule Hangman.Impl.Game do
   @spec new_game(String.t()) :: t
   def new_game(word) do
     %__MODULE__{
-      letters: word |>  String.codepoints()
+      letters: word |> String.codepoints()
     }
   end
 end
