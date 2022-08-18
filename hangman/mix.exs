9784739109cae538e200a093a998120c697153a3
Helder Burato Berto <helder.burato@gmail.com>
2022-08-18 11:31:03 +0100

    Add inflex to deps

--- hangman/mix.exs
+++ hangman/mix.exs
@@ -22,7 +22,8 @@ defmodule Hangman.MixProject do
   defp deps do
     [
       {:dictionary, path: "../dictionary"},
-      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:inflex, "~> 2.1"}
     ]
   end
 end
